10ee8194e6f0b1b03dc2bf0b76deb1d2cb1005bf
Denise Dube <denise.d.dube@gmail.com>
2021-07-06 15:57:23 +0100

    Denise, Amir, Ahmed

--- src/main/example.ts
+++ src/main/example.ts
@@ -1,5 +1,51 @@
-export class Example {
-    add(a: number, b: number) {
-        return a + b;
+export class TicTacToe {
+    constructor(
+        private board = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""],
+        ],
+        private isPlayerX = true
+    ) {
     }
+
+    play(position: Position) {
+
+        let positionX = Number(position.charAt(0));
+        let positionY = Number(position.charAt(2));
+        if (this.board[positionX][positionY]) {
+            throw new Error("Position Taken");
+        }
+        this.board[positionX][positionY] = this.isPlayerX
+            ? "X"
+            : "O";
+        this.isPlayerX = !this.isPlayerX;
+    }
+
+    printGrid(): string {
+        let printedBoard = "";
+
+        this.board.forEach((row) => {
+            printedBoard += "|";
+            row.forEach((position) => {
+                printedBoard += `${position}|`;
+            });
+
+            printedBoard += "\n";
+        });
+
+        return printedBoard;
+    }
+}
+
+export enum Position {
+    TopLeft = "0,0",
+    TopMiddle = "0,1",
+    TopRight = "0,2",
+    MiddleLeft = "1,0",
+    Centre = "1,1",
+    MiddleRight = "1,2",
+    BottomLeft = "2,0",
+    BottomMiddle = "2,1",
+    BottomRight = "2,2",
 }
--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,8 +1,49 @@
-import {Example} from "../main/example";
+import { Position, TicTacToe } from "../main/example";
 
-describe('example test', () => {
-    it("should give sum of a + b when adding a and b", () => {
-        let example: Example = new Example();
-        expect(example.add(1, 2)).toBe(3);
-    })
-})
+describe("Tic Tac Toe", () => {
+  it("should initialise the board", () => {
+    const tictactoe = new TicTacToe();
+    expect(tictactoe.printGrid()).toBe("||||\n||||\n||||\n");
+  });
+  it.each([
+    [Position.TopLeft, "|X|||\n||||\n||||\n"],
+    [Position.TopMiddle, "||X||\n||||\n||||\n"],
+    [Position.TopRight, "|||X|\n||||\n||||\n"],
+    [Position.MiddleLeft, "||||\n|X|||\n||||\n"],
+    [Position.Centre, "||||\n||X||\n||||\n"],
+    [Position.MiddleRight, "||||\n|||X|\n||||\n"],
+    [Position.BottomLeft, "||||\n||||\n|X|||\n"],
+    [Position.BottomMiddle, "||||\n||||\n||X||\n"],
+    [Position.BottomRight, "||||\n||||\n|||X|\n"],
+  ])(
+    "X plays first in %s to result in board matching \n%s",
+    (position: Position, expectedBoard: string) => {
+      const tictactoe = new TicTacToe();
+      tictactoe.play(position);
+      expect(tictactoe.printGrid()).toBe(expectedBoard);
+    }
+  );
+
+  it("should place the right piece on the second turn", () => {
+    const tictactoe = new TicTacToe();
+    tictactoe.play(Position.TopLeft);
+    tictactoe.play(Position.TopMiddle);
+    expect(tictactoe.printGrid()).toBe("|X|O||\n||||\n||||\n");
+  });
+
+  it("should place the right piece on the second turn", () => {
+    const tictactoe = new TicTacToe();
+    tictactoe.play(Position.TopLeft);
+    tictactoe.play(Position.TopMiddle);
+    tictactoe.play(Position.TopRight);
+    expect(tictactoe.printGrid()).toBe("|X|O|X|\n||||\n||||\n");
+  });
+
+  it("player cannot place a piece on an occupied grid", () => {
+    const tictactoe = new TicTacToe();
+    tictactoe.play(Position.TopLeft);
+    expect(() => tictactoe.play(Position.TopLeft)).toThrow(
+      new Error("Position Taken")
+    );
+  });
+});
